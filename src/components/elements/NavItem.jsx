d5d24bffab4b1e3e6c41cdd48da163204555a617
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-22 04:42:58 -0700

    remove log

--- src/components/elements/NavItem.jsx
+++ src/components/elements/NavItem.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import styles from "./navItem.module.css";
 
 export default function NavItem({ title, route, icon }) {
-  console.log(icon);
   return (
     <Link
       to={route}
