adec30b9d8c7ac56a7ca7df8898ff36365255fd2
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-22 04:48:30 -0700

    complete main header component

---
+++ src/components/modules/MainHeader.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import icon from "/src/favicon.ico";
+import Menu from "../elements/Menu";
+
+export default function MainHeader() {
+  return (
+    <header className="container mx-auto p-4 flex justify-between">
+      <Link
+        to="/"
+        className="flex items-center font-bold italic tracking-wide text-2xl"
+      >
+        Food
+        <img src={icon} className="mx-2 h-7" alt="Food" />
+        Mood
+      </Link>
+      <nav className="text-2xl font-semibold">
+        <Menu />
+      </nav>
+    </header>
+  );
+}
