a5c90571e68409e289df970f0a3ef6b4c2353d4a
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-23 15:24:31 -0700

    test image on the srver

--- src/components/constant/Images.js
+++ src/components/constant/Images.js
@@ -1,18 +1,18 @@
-import img1 from "../../assets/images/1.jpeg";
-import img2 from "../../assets/images/2.jpeg";
-import img3 from "../../assets/images/3.jpeg";
-import img4 from "../../assets/images/4.jpeg";
-import img5 from "../../assets/images/5.jpeg";
-import img6 from "../../assets/images/6.jpeg";
-import img7 from "../../assets/images/7.jpeg";
-import img8 from "../../assets/images/8.jpeg";
-import img9 from "../../assets/images/9.jpeg";
-import img10 from "../../assets/images/10.jpeg";
-import img11 from "../../assets/images/11.jpeg";
-import img12 from "../../assets/images/12.jpeg";
-import img13 from "../../assets/images/13.jpeg";
-import img14 from "../../assets/images/14.jpeg";
-import img15 from "../../assets/images/15.jpeg";
+import img1 from "/src/assets/images/1.jpeg";
+import img2 from "/src/assets/images/2.jpeg";
+import img3 from "/src/assets/images/3.jpeg";
+import img4 from "/src/assets/images/4.jpeg";
+import img5 from "/src/assets/images/5.jpeg";
+import img6 from "/src/assets/images/6.jpeg";
+import img7 from "/src/assets/images/7.jpeg";
+import img8 from "/src/assets/images/8.jpeg";
+import img9 from "/src/assets/images/9.jpeg";
+import img10 from "/src/assets/images/10.jpeg";
+import img11 from "/src/assets/images/11.jpeg";
+import img12 from "/src/assets/images/12.jpeg";
+import img13 from "/src/assets/images/13.jpeg";
+import img14 from "/src/assets/images/14.jpeg";
+import img15 from "/src/assets/images/15.jpeg";
 
 export const Images = [
   img1,
