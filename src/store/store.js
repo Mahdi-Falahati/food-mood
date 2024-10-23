8b1c7707fe9a8edf469a438256899045e7c8046a
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-23 01:10:39 -0700

    add food rducer to store rudcer

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import foodReducer from "./features/foodSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    food: foodReducer,
+  },
 });
