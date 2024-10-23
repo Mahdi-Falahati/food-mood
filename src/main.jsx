b9bb678c9deff8b9bf00a239f22d451de6df135e
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-23 01:12:47 -0700

    import swiper css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import "./index.css";
+import "swiper/css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
