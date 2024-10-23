0e352e5fbcb0add615fbb4a90fdf25e1cfd6c97a
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-23 03:29:10 -0700

    create slider for showing discounts

---
+++ src/components/modules/DiscountsSlider.jsx
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+import { EffectCube, Pagination } from "swiper/modules";
+import { Link } from "react-router-dom";
+import { RiLinksFill } from "react-icons/ri";
+
+export default function DiscountsSlider({ data }) {
+  return (
+    <div className="w-[260px] md:w-[380px] mb-20">
+      <Swiper
+        effect={"cube"}
+        grabCursor={true}
+        slidesPerView={1}
+        loop={true}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectCube, Pagination]}
+        className="mySwiper"
+      >
+        {data?.map((item, i) => {
+          console.log(item);
+          return (
+            <>
+              <SwiperSlide key={i}>
+                <div className="relative">
+                  <img
+                    src={`/src/assets/images/${item.id}.jpeg`}
+                    className="rounded-2xl"
+                  />
+                  <div
+                    className="text-white absolute top-0 z-10 px-2 py-3 left-0 right-0 rounded-t-2xl font-semibold tracking-wider"
+                    style={{ background: "#00000085" }}
+                  >
+                    <div className="flex justify-around flex-wrap items-center">
+                      <h2 className="text-xl inline-block my-1">{item.name}</h2>
+                      <span className="text-white my-1 bg-red-600 py-1 px-2 rounded-md">
+                        {item.discount} %
+                      </span>
+                    </div>
+                    <Link
+                      to={`/menu/${item.id}`}
+                      className="hover:scale-105 underline flex justify-center items-center font-semibold tracking-wider  mt-2 py-1"
+                    >
+                      <RiLinksFill className="mr-2 text-xl" />
+                      See More Details
+                    </Link>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+}
