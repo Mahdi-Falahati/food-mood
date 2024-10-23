00a1f6f6f1a488eac691a96bcefb97c325f46c8a
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-23 05:46:45 -0700

    fixed bug

--- src/components/elements/Title.jsx
+++ src/components/elements/Title.jsx
@@ -2,13 +2,11 @@ export default function Title({ title, icon, color }) {
   return (
     <h2
       className={`shadow-inner ${
-        color ? "text-" + color : "text-gray-700"
+        color ? color : "text-gray-700"
       } shadow-slate-400 w-[280px] px-2 py-2 rounded-md mb-5 italic flex items-center font-semibold tracking-wider text-2xl`}
     >
-      Discount Products
       {title}
       {icon ? icon : null}
-      <TbShoppingBagDiscount className="ml-2 text-3xl" />
     </h2>
   );
 }
