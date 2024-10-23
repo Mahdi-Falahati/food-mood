f0ff5ae0e85169ad3118104553aab04877ddb87a
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-23 12:38:19 -0700

    create cart slice

---
+++ src/store/features/cartSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  shoppingCart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      state.shoppingCart.forEach((item) => {
+        if (item.id === action.payload.id) {
+          item.count++;
+        } else {
+          state.shoppingCart.push({ ...item, count: 1 });
+        }
+      });
+    },
+    removeFromCart(state, action) {
+      const filtered = state.shoppingCart.filter((item) => {
+        if (item.id === action.payload) {
+          if (item.count >= 2) {
+            item.count--;
+          }
+        } else {
+          return item;
+        }
+      });
+      state.shoppingCart = filtered;
+    },
+
+    ClearCart(state) {
+      state.shoppingCart = [];
+    },
+  },
+});
+
+export default cartSlice.reducer;
+export { addToCart, removeFromCart, ClearCart };
