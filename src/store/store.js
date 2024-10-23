4fcb81149b84e62e98719dd29818caa8c045dada
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-23 12:39:20 -0700

    add cart reduce to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import foodReducer from "./features/foodSlice";
+import cartReducer from "./features/cartSlice";
 
 export const store = configureStore({
   reducer: {
     food: foodReducer,
+    cart: cartReducer,
   },
 });
