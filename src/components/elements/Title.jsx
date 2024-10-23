16a1064eac87128cfb8c9f39608cf9eb6b9fa4d7
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-23 05:49:00 -0700

    the component make responsive

--- src/components/elements/Title.jsx
+++ src/components/elements/Title.jsx
@@ -3,7 +3,7 @@ export default function Title({ title, icon, color }) {
     <h2
       className={`shadow-inner ${
         color ? color : "text-gray-700"
-      } shadow-slate-400 w-[280px] px-2 py-2 rounded-md mb-5 italic flex items-center font-semibold tracking-wider text-2xl`}
+      } shadow-slate-400 w-[260px] md:w-[300px] px-2 py-2 rounded-md mb-5 italic flex justify-center items-center font-semibold tracking-wider text-xl md:text-2xl`}
     >
       {title}
       {icon ? icon : null}
